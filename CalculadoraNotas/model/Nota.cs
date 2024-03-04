92f9849f20bf57b270014a9698b27c9658e169a0
daesvi <villegasdaniel4321@gmail.com>
2024-03-04 12:28:37 -0500

    ultimos retoques

--- CalculadoraNotas/model/Nota.cs
+++ CalculadoraNotas/model/Nota.cs
@@ -11,11 +11,17 @@ namespace CalculadoraNotas
         public string nombre {get; set;}
         public double porcentaje {get; set;}
         public double valor { get; set;}
-    public Nota(string nombre, double valor, double porcentaje)
+
+        public Nota(string nombre, double valor, double porcentaje)
         {
             this.nombre = nombre;
             this.valor = valor;
             this.porcentaje = porcentaje /100;
+        }
+
+        public override string ToString()
+        {
+            return ($"Nombre: {this.nombre}\nNota: {this.valor}\nPorcentaje: {this.porcentaje * 100}%\n");
         }
     }
 }
